644c77684b221798b4ab170c7a586e271bd3608a
Premiummalts <minjae2717@gmail.com>
2020-12-08 21:07:14 +0900

    일단 첫 업로드

---
+++ src/classes/Human.java
@@ -0,0 +1,22 @@
+package classes;
+
+public class Human {
+	String name;
+	int hp;
+	int mp;
+	int stamina;
+	
+	public void walk() {
+	}
+	
+	public void attack() {
+		
+	}
+	public void sleep() {
+		
+	}
+	
+	public void useItem() {
+		
+	}
+}
